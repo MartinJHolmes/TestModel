5abc1483d57b3450d80942906ff37cb1f6b14d55
MartinJHolmes <martin_j_holmes@hotmail.com>
2020-06-23 19:46:18 +0100

    Sleep time removed

--- TestModel/src/test/java/supportStatic/GlobalVariables.java
+++ TestModel/src/test/java/supportStatic/GlobalVariables.java
@@ -6,13 +6,13 @@ public class GlobalVariables {
 	public static String webDriverType = "webdriver.chrome.driver";
 	public static String webHomePage = "file:///c://Martin_Holmes_Files//Test HTML//Bank2.html";
 	public static String detailsPage = "file:///c://Martin_Holmes_Files//Test HTML//Bank4.html";
-	public static int sleepBetweenSteps = 500; //000;
-	public static boolean highlightWebElementRequired = true;
+	public static int sleepBetweenSteps = 500; //500;
+	public static boolean highlightWebElementRequired = false;
 	public static int highlightTime = 500;
-	public static Boolean debugOn = true;
+	public static Boolean debugOn = false;
 	public static int threadCount = 0;
 	public static String fieldMapLocation = "C:\\Martin_Holmes_Files\\Test HTML\\Website\\fieldMap.txt";
-	public static Boolean noSleep = false;
+	public static Boolean noSleep = true;
 	public static Boolean failOnError = false;
 	public static int implicitWait = 1;
 	public static String applicationType = "Not-REACT";
